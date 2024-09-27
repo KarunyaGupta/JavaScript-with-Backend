ad852517e4d2b31f947ebd1cc8f6cdfe1823d921
KarunyaGupta <mails.karunyagupta@gmail.com>
2024-09-27 17:08:28 +0530

    make folders and files

---
+++ Intermediate JavaScript/Interpreted/script.js
@@ -0,0 +1,5 @@
+alert("Hello");
+alert("World!"); // give me pop-up alert
+console.log("Hello World!"); // give me console log
+confirm("Do you want to proceed?"); // give me confirm alert with OK and Cancel button and return true or false
+prompt("What is your name?"); // give me prompt alert with OK and Cancel button and return the value entered by user
